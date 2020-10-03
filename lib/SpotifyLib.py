673bb2f9bd5e5a061b941ea02df113f34a810ba3
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-10-02 22:59:36 -0400

    Moved SpotifyLib from TwitchHelper.

---
+++ lib/SpotifyLib.py
@@ -0,0 +1,19 @@
+import spotipy
+
+from Classes import Song
+
+
+def get_spotipy_client(client_id, client_secret, scope, redirect_uri):
+    auth_manager = spotipy.SpotifyOAuth(
+        client_id=client_id,
+        client_secret=client_secret,
+        scope=scope,
+        redirect_uri=redirect_uri)
+    return spotipy.Spotify(auth_manager=auth_manager)
+
+
+def get_currently_playing_song(spotipy_client):
+    current_song_dict = spotipy_client.currently_playing()
+    return Song.Song(
+        current_song_dict=current_song_dict
+    )
