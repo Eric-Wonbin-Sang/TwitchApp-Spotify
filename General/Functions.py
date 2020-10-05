e9de40ca6a7ef47f38a25de9981b5d7b2d37c306
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-10-04 22:49:11 -0400

    Added alter_window function in Functions.py.

--- General/Functions.py
+++ General/Functions.py
@@ -1,5 +1,6 @@
 import os
 import json
+import win32gui
 
 
 def str_to_length(base_str, length, do_dots=True):
@@ -26,3 +27,20 @@ def get_pretty_time(some_datetime):
 
 def tab_str(some_str, tab_count):
     return ("\t" * tab_count) + some_str.replace("\n", "\n" + ("\t" * tab_count))
+
+
+def alter_window(hwnd, x=None, y=None, x_delta=None, y_delta=None,
+                 width=None, height=None, width_delta=None, height_delta=None):
+    bbox = win32gui.GetWindowRect(hwnd)
+    window_x, window_y = bbox[0], bbox[1]
+
+    new_x = x if x is not None else window_x + x_delta
+    new_y = y if y is not None else window_y + y_delta
+    new_width = width if width is not None else width + width_delta
+    new_height = height if height is not None else height + height_delta
+    if new_width < 10:
+        new_width = 10
+    if new_height < 10:
+        new_height = 10
+    
+    win32gui.MoveWindow(hwnd, new_x, new_y, new_width, new_height, True)
