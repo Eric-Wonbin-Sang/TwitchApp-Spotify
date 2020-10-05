46f30d028e4f53cd1082d8e990d8d3f015bb5ba7
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-10-04 22:48:54 -0400

    Added song_update_time and window_adjust to Constants.py.

--- General/Constants.py
+++ General/Constants.py
@@ -2,3 +2,6 @@ from General import Functions
 
 
 spotify_credentials_file_path = Functions.get_curr_parent_dir() + "/0 - Secrets/TwitchHelper/spotify_credentials.json"
+
+song_update_time = 0.5
+window_adjust = 2
