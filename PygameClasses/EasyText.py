c09ee8ab847d79a3224f6a025aea60735befa3bc
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-10-14 20:57:00 -0400

    EasyText.py - saved the surface and rect generated from the font objects.

--- PygameClasses/EasyText.py
+++ PygameClasses/EasyText.py
@@ -22,18 +22,25 @@ class EasyText(Font):
         self.draw_center = kwargs.get("draw_center", True)
         self.draw_from_bottom = kwargs.get("draw_from_bottom", False)
 
+        self.surface, self.rect = self.get_surface_and_rect()
+
     def update_coordinates(self, x, y):
         self.x = x
         self.y = y
 
-    def draw(self, screen):
+    def get_surface_and_rect(self):
         text_surface, rect = self.render(self.text, self.color)
-        text_surface.set_alpha(self.opacity)
+        return text_surface, rect
+
+    def draw(self, screen):
+        self.surface, self.rect = self.get_surface_and_rect()
+        self.surface.set_alpha(self.opacity)
 
         x_offset = 0
         y_offset = 0
 
         if self.draw_center:
-            screen.blit(text_surface, (self.x + x_offset - rect.width / 2, self.y + y_offset - rect.height + self.size/2))
+            screen.blit(self.surface,
+                        (self.x + x_offset - self.rect.width / 2, self.y + y_offset - self.rect.height + self.size/2))
         else:
-            screen.blit(text_surface, (self.x + x_offset, self.y + y_offset))
+            screen.blit(self.surface, (self.x + x_offset, self.y + y_offset))
