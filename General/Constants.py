c5e69454b3abc5a87477259f698386a3ead13bac
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-10-14 20:56:30 -0400

    Constants.py - changed the song_update_time to milliseconds from seconds.

--- General/Constants.py
+++ General/Constants.py
@@ -3,5 +3,5 @@ from General import Functions
 
 spotify_credentials_file_path = Functions.get_curr_parent_dir() + "/0 - Secrets/TwitchHelper/spotify_credentials.json"
 
-song_update_time = 0.5
+song_update_time = 0.2 * 1000000
 window_adjust = 2
